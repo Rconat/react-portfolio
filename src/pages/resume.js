dcf3fca869652286a8b43a306dcf46d1731e4e12
ryzecous <r3j9c89@gmail.com>
2021-04-05 23:23:57 -0400

    resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,7 +1,93 @@
 import React from 'react'
 
 const Resume = () => (
-    <div>RESUME</div>
+    <div className="container">
+        <section id="portfolio">
+            <h2>R&eacute;sum&eacute;</h2>
+            <hr/>
+            <div className="row">
+                <div className="col-sm-3" id="category">
+                    <h2>Education</h2>
+                </div>
+                <div class="col-sm-9" id="education">
+                    <h2>2021 (Upcoming!)</h2>
+                    <ul>
+                        <li>Certificate of Completion - Full Stack developer</li>
+                        <li>Northwestern University - Evanston, Illinois</li>
+                    </ul>
+                    <h2>2012</h2>
+                    <ul>
+                        <li>Bachelor of Science in Design</li>
+                        <li>University of Nebraska Lincoln - Lincoln, Nebraska</li>
+                    </ul>
+                    <h2>2007</h2>
+                    <ul>
+                        <li>Graduated from Glenbrook North High School - Northbrook, Illinois</li>
+                    </ul>
+                    <br/>
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-sm-3" id="category">
+                    <h2>Experience</h2>
+                </div>
+                <div class="col-sm-9" id="experience">
+                    <h2>February 2019</h2>
+                    <ul>
+                        <li>Pipeworks Brewing Company</li>
+                        <li>Sales Team</li>
+                    </ul>
+                    <h2>November 2018</h2>
+                    <ul>
+                        <li>Swoon Living</li>
+                        <li>Laser Cutter Technician/Shop Assistant</li>
+                    </ul>
+                    <h2>May 2017</h2>
+                    <ul>
+                        <li>Left Hand Brewing Co.</li>
+                        <li>Chicago Sales Representative</li>
+                    </ul>
+                    <h2>May 2016</h2>
+                    <ul>
+                        <li>Whole Foods - Evanston</li>
+                        <li>Specialty Beverage Buyer</li>
+                    </ul>
+                    <h2>November 2013</h2>
+                    <ul>
+                        <li>Binny's Beverage Depot - Highland Park</li>
+                        <li>Beer Buyer/Customer Service Manager</li>
+                    </ul>
+                    <br/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3" id="category">
+                    <h2>Proficiencies</h2>
+                </div>
+                <div class="col-sm-9" id="proficiencies">
+                <h2>Coding</h2>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                        <li>jQuery</li>
+                        <li>Node.js</li>
+                        <li>SQL Databases</li>
+                        <li>NoSQL Databases</li>
+                    </ul>
+                <h2>Adobe Creative Suite</h2>
+                    <ul>
+                        <li>Photoshop</li>
+                        <li>Illustrator</li>
+                        <li>InDesign</li>
+                    </ul>
+                <br/>
+                </div>
+            </div>
+        </section>
+    </div>
 )
 
 export default Resume
