ec02c3348592fad0d54736061ba82d5da59fd57d
ryzecous <r3j9c89@gmail.com>
2021-04-06 15:19:46 -0400

    link text styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
             <h1 className="navbar-brand" href="#">Coding Portfolio</h1>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,7 +18,7 @@ const Nav = () => {
                 <Link to="/projects" className="nav-link" href="#">Projects</Link>
                 </li>
                 <li className="nav-item">
-                <Link to="/resume" className="nav-link" href="#">Resume</Link>
+                <Link to="/resume" className="nav-link" href="#">R&eacute;sum&eacute;</Link>
                 </li>
                 <li className="nav-item">
                 <Link to="/contact" className="nav-link" href="#">Contact</Link>
