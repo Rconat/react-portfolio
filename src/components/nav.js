95754a01205d3c903d3c62631abb40cad69d2526
ryzecous <r3j9c89@gmail.com>
2021-11-04 22:22:27 -0400

    updated navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-            <h1 className="navbar-brand" href="#">Coding Portfolio</h1>
+            <h1 className="navbar-brand" href="#">Full Stack Web Developer</h1>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
